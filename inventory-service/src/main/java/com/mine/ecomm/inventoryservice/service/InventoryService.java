ba7e2a2e6a9c0b743579747f6705701ffdc5732d
Thiru <pthirumalayadav@gmail.com>
2024-01-08 13:40:38 +0530

    Added business logic for add quantity of stock in inventory service.

--- inventory-service/src/main/java/com/mine/ecomm/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/mine/ecomm/inventoryservice/service/InventoryService.java
@@ -1,14 +1,18 @@
 package com.mine.ecomm.inventoryservice.service;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.mine.ecomm.inventoryservice.entity.Inventory;
 import com.mine.ecomm.inventoryservice.repository.InventoryRepository;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
@@ -16,5 +20,21 @@ public class InventoryService {
     @Transactional(readOnly = true)
     public boolean isInStock(String skuCode) {
         return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    }
+
+    public boolean addStockQuantity(String skuCode, int quantity) {
+        final Optional<Inventory> OptInventory = inventoryRepository.findBySkuCode(skuCode);
+        if (OptInventory.isPresent()) {
+            final Inventory inventory = OptInventory.get();
+            inventory.setQuantity(inventory.getQuantity() + quantity);
+            inventoryRepository.saveAndFlush(inventory);
+            return true;
+        }
+        final Inventory inventory = Inventory.builder()
+                .skuCode(skuCode)
+                .quantity(quantity)
+                .build();
+        inventoryRepository.saveAndFlush(inventory);
+        return true;
     }
 }
