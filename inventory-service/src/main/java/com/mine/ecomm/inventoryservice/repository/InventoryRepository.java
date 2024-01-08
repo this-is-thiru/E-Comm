bf42afb226f9db0a70719e4b211eb7b603683aed
Thiru <pthirumalayadav@gmail.com>
2024-01-08 22:29:31 +0530

    Changes in Inventory Service for list of skuCodes. Whether they are in stock or not.

--- inventory-service/src/main/java/com/mine/ecomm/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/mine/ecomm/inventoryservice/repository/InventoryRepository.java
@@ -1,11 +1,14 @@
 package com.mine.ecomm.inventoryservice.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mine.ecomm.inventoryservice.entity.Inventory;
 
-import java.util.Optional;
-
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
     Optional<Inventory> findBySkuCode(final String skuCode);
+
+    List<Inventory> findBySkuCodeIn(final List<String> skuCodes);
 }
--- inventory-service/src/main/java/com/mine/ecomm/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/mine/ecomm/inventoryservice/service/InventoryService.java
@@ -1,10 +1,12 @@
 package com.mine.ecomm.inventoryservice.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.mine.ecomm.inventoryservice.dto.InventoryResponse;
 import com.mine.ecomm.inventoryservice.entity.Inventory;
 import com.mine.ecomm.inventoryservice.repository.InventoryRepository;
 
@@ -18,8 +20,14 @@ public class InventoryService {
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
-    public boolean isInStock(String skuCode) {
-        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    public List<InventoryResponse> isInStock(List<String> skuCodes) {
+        return inventoryRepository.findBySkuCodeIn(skuCodes).stream()
+                .map(inventory ->
+                        InventoryResponse.builder()
+                                .skuCode(inventory.getSkuCode())
+                                .isInStock(inventory.getQuantity() > 0)
+                                .build()
+                ).toList();
     }
 
     public boolean addStockQuantity(String skuCode, int quantity) {
