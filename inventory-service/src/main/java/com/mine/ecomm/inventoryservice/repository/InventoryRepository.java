3970401b80d468c3247f977875bd6876792c0da4
Thiru <pthirumalayadav@gmail.com>
2024-01-08 22:10:10 +0530

    Changes in Inventory Service for list of skuCodes. Whether they are in stock or not.

--- inventory-service/src/main/java/com/mine/ecomm/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/mine/ecomm/inventoryservice/repository/InventoryRepository.java
@@ -1,11 +1,12 @@
 package com.mine.ecomm.inventoryservice.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mine.ecomm.inventoryservice.entity.Inventory;
 
-import java.util.Optional;
-
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
-    Optional<Inventory> findBySkuCode(final String skuCode);
+
+    List<Inventory> findBySkuCodeIn(final List<String> skuCodes);
 }
