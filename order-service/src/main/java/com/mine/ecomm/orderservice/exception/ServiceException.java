6cfd2e7d66563f3e96cd5f617af4db7c41cf82e2
Thiru <pthirumalayadav@gmail.com>
2024-02-28 19:03:41 +0530

    Add order-service submodule

--- order-service/src/main/java/com/mine/ecomm/orderservice/exception/ServiceException.java
+++
@@ -1,8 +0,0 @@
-package com.mine.ecomm.orderservice.exception;
-
-public class ServiceException extends RuntimeException {
-    public ServiceException(String msg) {
-        super(msg);
-    }
-
-}
