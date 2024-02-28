5c96acbfcc1800446d4477ac5a4a16700a9ee915
Thiru <pthirumalayadav@gmail.com>
2024-02-28 19:23:11 +0530

    Add wishlist-service submodule

--- wishlist-server/src/main/java/com/mine/ecomm/wishlistservice/repository/WishlistRepo.java
+++
@@ -1,8 +0,0 @@
-package com.mine.ecomm.wishlistservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mine.ecomm.wishlistservice.entity.Wishlist;
-
-public interface WishlistRepo extends JpaRepository<Wishlist, String> {
-}
